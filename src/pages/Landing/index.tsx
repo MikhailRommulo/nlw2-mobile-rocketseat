f8c5d9f359b39395b4b01400fdc7147f59ea4123
MikhailRommulo <mikhailrommulo@gmail.com>
2020-08-09 13:54:55 -0300

    aula 5

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
+
+import api from '../../services/api';
 
 import styles from './styles';
 
@@ -11,6 +13,16 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
 function Landing() {
+    const [totalConnections, setTotalConnections] = useState<number>(0);
+
+    useEffect(() => {
+      api.get('connections').then(res => {
+        const { total } = res.data;
+
+        setTotalConnections(total);
+      })
+    }, []);
+
     const { navigate } = useNavigation();
 
     function handleNavigateToGiveClassesPage() {
@@ -51,7 +63,7 @@ function Landing() {
           </View>
 
           <Text style={styles.totalConnections}>
-            Total de 285 conexões já realizadas {' '}
+            Total de {totalConnections} conexões já realizadas {' '}
             <Image source={heartIcon} />
           </Text>
         </View>
